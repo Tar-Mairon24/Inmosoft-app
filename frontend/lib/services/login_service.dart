18712d050875517e2f4f2d86331a8521c52754ac
Tar-Mairon24 <Marioirigoyen24@outlook.com>
2024-10-08 12:42:41 -0600

    correcciones de ruta y logs

--- frontend/lib/services/login_service.dart
+++ frontend/lib/services/login_service.dart
@@ -1,12 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 class DioService {
   final Dio _dio = Dio();
+  final Logger log = Logger();
 
   Future<bool> login(String email, String password) async {
     try {
       final response = await _dio.post(
-        'https://localhost:8080/login',
+        'http://localhost:8080/login',
         data: {
           'email': email,
           'password': password,
@@ -14,13 +16,29 @@ class DioService {
       );
 
       if (response.statusCode == 200) {
-        // Assuming the API returns a success status
+        log.i('Login successful for email: $email');
         return true;
       } else {
+        log.w('Login failed for email: $email');
         return false;
       }
+    } on DioException catch (e) {
+      if (e.type == DioExceptionType.connectionTimeout) {
+        log.e('Connection Timeout Exception');
+      } else if (e.type == DioExceptionType.sendTimeout) {
+        log.e('Send Timeout Exception');
+      } else if (e.type == DioExceptionType.receiveTimeout) {
+        log.e('Receive Timeout Exception');
+      } else if (e.type == DioExceptionType.badResponse) {
+        log.e('Received invalid status code: ${e.response?.statusCode}');
+      } else if (e.type == DioExceptionType.cancel) {
+        log.e('Request to API server was cancelled');
+      } else if (e.type == DioExceptionType.unknown) {
+        log.e('Unexpected error: ${e.message}');
+      }
+      return false;
     } catch (e) {
-      print('Error: $e');
+      ('Unexpected error: $e');
       return false;
     }
   }
