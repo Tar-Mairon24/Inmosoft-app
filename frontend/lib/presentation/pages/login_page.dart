227b3323cc6c68549773b2efb902246a2ff86a41
Tar-Mairon24 <Marioirigoyen24@outlook.com>
2024-10-13 15:53:16 -0600

    inserts de propiedades

--- frontend/lib/presentation/pages/login_page.dart
+++ frontend/lib/presentation/pages/login_page.dart
@@ -95,7 +95,7 @@ class LoginPage extends StatelessWidget {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => HomePage(),
+                                  builder: (context) => HomePage(email: emailController.text),
                                 ),
                               );
                             } else {
