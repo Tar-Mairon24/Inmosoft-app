93e244084d0d19f5e1bb6b44e53c806a43263f64
Llanas21 <joseluisllanas_21@outlook.com>
2024-10-21 10:41:12 -0600

    Primer avance en AgreementAdderPage

---
+++ frontend/lib/presentation/widgets/add_agreement_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AddAgreementWidget extends StatelessWidget {
+  const AddAgreementWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(border: Border.all()),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.01),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add,
+                  color: Theme.of(context).iconTheme.color,
+                  size: MediaQuery.of(context).size.height * 0.1,
+                ),
+                Text(
+                  "Agregar contrato",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).primaryTextTheme.bodyMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+}
