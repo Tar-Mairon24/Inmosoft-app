1b95edeba53c0168c9467b454a0823d02a4d547b
Llanas21 <joseluisllanas_21@outlook.com>
2024-10-28 10:56:33 -0600

    Añadí el insertPropiedad en lib/services/propiedad_service.dart, no sé si esté bien

--- frontend/lib/services/propiedad_service.dart
+++ frontend/lib/services/propiedad_service.dart
@@ -24,7 +24,9 @@ class PropiedadService {
         log.i('Propiedades fetched successfully');
         return Result(
           success: true,
-          data: propiedades.map((propiedad) => PropiedadMenu.fromJson(propiedad)).toList(),
+          data: propiedades
+              .map((propiedad) => PropiedadMenu.fromJson(propiedad))
+              .toList(),
         );
       } else {
         errorMessage = 'Failed to get propiedades: ${response.statusCode}';
@@ -39,7 +41,8 @@ class PropiedadService {
       } else if (e.type == DioExceptionType.receiveTimeout) {
         errorMessage = 'Receive Timeout Exception';
       } else if (e.type == DioExceptionType.badResponse) {
-        errorMessage = 'Received invalid status code: ${e.response?.statusCode}';
+        errorMessage =
+            'Received invalid status code: ${e.response?.statusCode}';
       } else if (e.type == DioExceptionType.cancel) {
         errorMessage = 'Request to API server was cancelled';
       } else if (e.type == DioExceptionType.unknown) {
@@ -59,7 +62,8 @@ class PropiedadService {
   Future<Result<Propiedad>> getPropiedad(int idPropiedad) async {
     String? errorMessage;
     try {
-      final response = await _dio.get('http://localhost:8080/propiedad/$idPropiedad');
+      final response =
+          await _dio.get('http://localhost:8080/propiedad/$idPropiedad');
       if (response.statusCode == 200) {
         final propiedad = Propiedad.fromJson(response.data);
         log.i('Propiedad fetched successfully');
@@ -77,7 +81,8 @@ class PropiedadService {
       } else if (e.type == DioExceptionType.receiveTimeout) {
         errorMessage = 'Receive Timeout Exception';
       } else if (e.type == DioExceptionType.badResponse) {
-        errorMessage = 'Received invalid status code: ${e.response?.statusCode}';
+        errorMessage =
+            'Received invalid status code: ${e.response?.statusCode}';
       } else if (e.type == DioExceptionType.cancel) {
         errorMessage = 'Request to API server was cancelled';
       } else if (e.type == DioExceptionType.unknown) {
@@ -93,5 +98,22 @@ class PropiedadService {
       return Result(success: false, errorMessage: errorMessage);
     }
   }
-}
 
+  Future<int> insertPropiedad(Propiedad propiedad) async {
+    try {
+      final response = await _dio.post(
+        'http://localhost:8080/propiedad',
+        data: propiedad.toJson(),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        log.i('Propiedades fetched successfully');
+      } else {
+        throw Exception("Failed to insert propiedad");
+      }
+    } catch (e) {
+      print("Error inserting propiedad: $e");
+      return 0; // Devuelve 0 en caso de error
+    }
+  }
+}
