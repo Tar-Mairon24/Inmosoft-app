e185661fb2b6dec06aeb6f331dd9509aaad2c5b8
Llanas21 <joseluisllanas_21@outlook.com>
2024-11-04 09:57:56 -0600

    Modifiqué property_widget

--- frontend/lib/presentation/widgets/property_widget.dart
+++ frontend/lib/presentation/widgets/property_widget.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/estado_propiedad_modelo.dart';
+import 'package:frontend/models/propiedad_menu_modelo.dart';
 import 'package:frontend/presentation/navigator_key.dart';
 import 'package:frontend/presentation/pages/detailed_property_page.dart';
+import 'package:frontend/presentation/pages/property_modifier_page.dart';
+import 'package:frontend/presentation/providers/properties_notifier.dart';
 import 'package:frontend/services/estado_propiedad_service.dart';
 import 'package:frontend/services/propiedad_service.dart';
 import 'package:provider/provider.dart';
 
 class PropertyWidget extends StatelessWidget {
+  // final Image image;
+  // final String title;
+  // final String status;
+  // final double price;
+  final PropiedadMenu property;
   final Image image;
-  final String title;
-  final String status;
-  final double price;
 
   const PropertyWidget({
     super.key,
+    required this.property,
     required this.image,
-    required this.title,
-    required this.status,
-    required this.price,
   });
 
   @override
@@ -26,8 +29,10 @@ class PropertyWidget extends StatelessWidget {
     final EstadoPropiedadService estadoPropiedadService =
         EstadoPropiedadService();
     return GestureDetector(
-      onTap: () => Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => DetailedPropertyPage())),
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => DetailedPropertyPage(
+                propertyID: property.idPropiedad,
+              ))),
       child: Container(
         decoration: BoxDecoration(
           border: Border.all(),
@@ -52,17 +57,17 @@ class PropertyWidget extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              title,
+                              property.titulo,
                               style:
                                   Theme.of(context).primaryTextTheme.titleSmall,
                             ),
                             Text(
-                              status,
+                              property.estado,
                               style:
                                   Theme.of(context).primaryTextTheme.bodySmall,
                             ),
                             Text(
-                              "$price MXN",
+                              "${property.precio} MXN",
                               style:
                                   Theme.of(context).primaryTextTheme.bodySmall,
                             ),
@@ -72,7 +77,13 @@ class PropertyWidget extends StatelessWidget {
                       PopupMenuButton<String>(
                         itemBuilder: (BuildContext context) => [
                           PopupMenuItem<String>(
-                            onTap: () {},
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => PropertyModifierPage(
+                                  propertyID: property.idPropiedad,
+                                ),
+                              ),
+                            ),
                             child: const Text('Editar'),
                           ),
                           PopupMenuItem<String>(
