3067dc7d5d199e3ab5c7ba67b5d90cadebe0523a
Llanas21 <joseluisllanas_21@outlook.com>
2024-12-01 01:20:46 -0600

    Ya se inicia en pantalla completa en windows

--- frontend/windows/runner/main.cpp
+++ frontend/windows/runner/main.cpp
@@ -25,11 +25,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+
+  // Obtén el tamaño de la pantalla principal
+  HMONITOR monitor = MonitorFromWindow(nullptr, MONITOR_DEFAULTTOPRIMARY);
+  MONITORINFO monitor_info = {sizeof(monitor_info)};
+  GetMonitorInfo(monitor, &monitor_info);
+
+  // Configurar la ventana para que ocupe toda la pantalla
+  Win32Window::Point origin(0, 0);
+  Win32Window::Size size(
+      monitor_info.rcMonitor.right - monitor_info.rcMonitor.left,
+      monitor_info.rcMonitor.bottom - monitor_info.rcMonitor.top);
+
   if (!window.Create(L"frontend", origin, size)) {
     return EXIT_FAILURE;
   }
+
+  // Eliminar bordes y hacer la ventana en pantalla completa
+  SetWindowLong(window.GetHandle(), GWL_STYLE, WS_POPUP | WS_VISIBLE);
+  ShowWindow(window.GetHandle(), SW_MAXIMIZE);
+
   window.SetQuitOnClose(true);
 
   ::MSG msg;
